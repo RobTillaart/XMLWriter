1231b900d17b257d94ea4893f88edb9382004d19
rob tillaart <rob.tillaart@gmail.com>
2021-01-09 23:39:02 +0100

    fix again

--- examples/XMLWriterTest/XMLWriterTest.ino
+++ examples/XMLWriterTest/XMLWriterTest.ino
@@ -70,24 +70,25 @@ void Weather()
   XML.tagClose();
 }
 
-void AnalogPorts(const char* name)
+// casting to keep some compilers happy
+void AnalogPorts(XMLWriter* xw, const char* name)
 {
-  XML.comment("The analog ports are multiplexed");
-  XML.tagOpen("Analog", name);
-  XML.writeNode("Analog0", itoa(analogRead(A0), buffer, 10));
-  XML.writeNode("Analog1", analogRead(A1));
-  XML.writeNode("Analog2", (5.0 * analogRead(A2)) / 1023); // default nr decimals = 2
-  XML.writeNode("Analog3", (5.0 * analogRead(A2)) / 1023, 3);
-  XML.tagClose();
+  xw->comment("The analog ports are multiplexed");
+  xw->tagOpen("Analog", name);
+  xw->writeNode("Analog0", itoa(analogRead(A0), buffer, 10));
+  xw->writeNode("Analog1", (uint16_t) analogRead(A1));
+  xw->writeNode("Analog2", (double) (5.0*analogRead(A2))/1023);  // default nr decimals = 2
+  xw->writeNode("Analog3", (double) (5.0*analogRead(A2))/1023, (uint8_t)3);
+  xw->tagClose();
 }
 
-void DigitalPorts()
+void DigitalPorts(XMLWriter* xw)
 {
-  XML.comment("The digital ports are not multiplexed");
-  XML.tagOpen("Digital");
-  XML.writeNode("D1", itoa(digitalRead(1), buffer, 10));
-  XML.writeNode("D13", digitalRead(13));
-  XML.tagClose();
+  xw->comment("The digital ports are not multiplexed");
+  xw->tagOpen("Digital");
+  xw->writeNode("D1", itoa(digitalRead(1), buffer, 10));
+  xw->writeNode("D13", (uint8_t)digitalRead(13));
+  xw->tagClose();
 }
 
 void DataTypes()
