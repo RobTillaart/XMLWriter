6bbedc30509538f03d46e65f047176600fe532b9
rob tillaart <rob.tillaart@gmail.com>
2021-01-09 22:11:19 +0100

    fix

--- examples/XMLWriterDefaultSerial/XMLWriterDefaultSerial.ino
+++ examples/XMLWriterDefaultSerial/XMLWriterDefaultSerial.ino
@@ -73,9 +73,9 @@ void AnalogPorts(const char* name)
   XML.comment("The analog ports are multiplexed");
   XML.tagOpen("Analog", name);
   XML.writeNode("Analog0", itoa(analogRead(A0), buffer, 10));
-  XML.writeNode("Analog1", analogRead(A1));
-  XML.writeNode("Analog2", (5.0 * analogRead(A2)) / 1023); // default nr decimals = 2
-  XML.writeNode("Analog3", (5.0 * analogRead(A2)) / 1023, 3);
+  XML.writeNode("Analog1", (uint16_t) analogRead(A1));
+  XML.writeNode("Analog2", (double) (5.0 * analogRead(A2)) / 1023); // default nr decimals = 2
+  XML.writeNode("Analog3", (double) (5.0 * analogRead(A3)) / 1023, 3);
   XML.tagClose();
 }
 
@@ -84,7 +84,7 @@ void DigitalPorts()
   XML.comment("The digital ports are not multiplexed");
   XML.tagOpen("Digital");
   XML.writeNode("D1", itoa(digitalRead(1), buffer, 10));
-  XML.writeNode("D13", digitalRead(13));
+  XML.writeNode("D13", (uint8_t) digitalRead(13));
   XML.tagClose();
 }
 
