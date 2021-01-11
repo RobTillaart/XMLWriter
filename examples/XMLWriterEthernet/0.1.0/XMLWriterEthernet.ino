d8723ea4513e277982ad0122b8e0025e59af7ae4
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-11 14:05:43 +0100

    Arduino ci (#8)
    * add arduino-ci
    * add unit tests
    * add add getIndentSize() + version() + debug();

--- examples/XMLWriterEthernet/0.1.0/XMLWriterEthernet.ino
+++ examples/XMLWriterEthernet/0.1.0/XMLWriterEthernet.ino
@@ -1,10 +1,10 @@
 //
 //    FILE: XMLWriterEthernet.ino
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.1
+// VERSION: 0.1.0
 // PURPOSE: demo XML writer for EthernetClient
 //    DATE: 2020-04-24
-//     URL: https://github.com/RobTillaart/XMLWriter
+//     URL: 
 //
 
 #include <XMLWriter.h>
@@ -32,7 +32,7 @@ void HTTP_header(EthernetClient* cl, const char *contentType, bool keepAlive = f
   cl->print("Content-Type: ");
   cl->println( contentType  );
   cl->println("Connection: ");
-  cl->println(keepAlive ? "keep-alive" : "close");
+  cl->println(keepAlive ? "keep-alive":"close");
   cl->println("Refresh: ");
   cl->println(refresh);
   cl->println();
@@ -60,9 +60,6 @@ void setup()
     while (1);
   }
 
-  Serial.println(Ethernet.hardwareStatus());
-  Serial.println(Ethernet.linkStatus());
-
   if (Ethernet.linkStatus() == LinkOFF)
   {
     Serial.println("Cable is not connected.");
@@ -84,9 +81,6 @@ void loop()
   if (client)
   {
     Serial.print("\n<CONNECTION>\n  ");
-    // Serial.println(client.remoteIP());
-    // Serial.println(client.remotePort());
-
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     reqCnt = 0;
@@ -107,66 +101,54 @@ void loop()
         // so you can send a reply
         if (c == '\n' && currentLineIsBlank)
         {
-          if (strstr(httpRequest, "1.xml"))
-          {
-            uint32_t start = micros();
+		  if (strstr(httpRequest, "1.xml"))
+		  {
             // send a standard http response header
             HTTP_header(&client, "text/xml", true, 5);
-            XMLWriter XML(&client, 250);
-
+            XMLWriter XML(&client);
+	   
             // XML body
             XML.header();
             XML.comment("XMLWriterTest.ino\nThis is a demo of a simple XML lib for Arduino", true);
-
+            
             // use of {} to get indentation that follows the XML (sort of)
             // it adds no code size, but improves readability a lot
             XML.tagOpen("Arduino", "42");
             {
               XML.tagOpen("Ports");
               {
-                AnalogPorts(&XML, "before");
-                DigitalPorts(&XML);
-                AnalogPorts(&XML, "after");
+                AnalogPorts(XML, "before");
+                DigitalPorts(XML);
+                AnalogPorts(XML,"after");
               }
               XML.tagClose();
             }
             XML.tagClose();
-            XML.flush();
-            uint32_t stop = micros();
-            Serial.println(stop - start);
-            Serial.println(XML.bytesWritten());
-            Serial.println(XML.bytesWritten() * 1e6 / (stop - start));
-            break;
-          }
-          if (strstr(httpRequest, "2.xml"))
-          {
-            uint32_t start = micros();
+		    break;
+		  }
+		  if (strstr(httpRequest, "2.xml"))
+		  {
             // send a standard http response header
             HTTP_header(&client, "text/xml", true, 5);
-            XMLWriter XML(&client, 250);
-
+            XMLWriter XML(&client);
+			
             // XML body
             XML.header();
             XML.comment("XMLWriterTest.ino\nThis is a demo of a simple XML lib for Arduino", true);
-
+          
             // use of {} to get indentation that follows the XML (sort of)
             // it adds no code size, but improves readability a lot
             XML.tagOpen("Arduino", "102");
             {
-              Weather(&XML);
-              Weather2(&XML);
-              DataTypes(&XML);
+              Weather(XML);
+              Weather2(XML);
+              DataTypes(XML);
             }
             XML.tagClose();
-            XML.flush();
-            uint32_t stop = micros();
-            Serial.println(stop - start);
-            Serial.println(XML.bytesWritten());
-            Serial.println(XML.bytesWritten() * 1e6 / (stop - start));
-            break;
-          }
-
-          // default page is simple HTML
+		    break;
+		  }
+		  
+		  // default page is simple HTML
           // send a standard http response header
           HTTP_header(&client, "text/html", true, 5);
 
@@ -205,85 +187,84 @@ void loop()
 //
 // XML CODE
 //
-void Weather2(XMLWriter* xw)
+void Weather2(XMLWriter xw)
 {
-  xw->comment("The weather in South Africa");
+  xw.comment("The weather in South Africa");
   for (int i = 0; i < 10; i++)
   {
-    xw->tagStart("Weather");
-    xw->tagField("Date", "20131106");
-    xw->tagField("Time", "1:42");
-    xw->tagField("Temp", "23.4");
-    xw->tagField("Humi", "50%");
-    xw->tagField("Rain", "10mm");
-    xw->tagField("Sun", "40");
-    xw->tagEnd();
+    xw.tagStart("Weather");
+    xw.tagField("Date", "20131106");
+    xw.tagField("Time", "1:42");
+    xw.tagField("Temp", "23.4");
+    xw.tagField("Humi", "50%");
+    xw.tagField("Rain", "10mm");
+    xw.tagField("Sun", "40");
+    xw.tagEnd();
   }
 }
 
-void Weather(XMLWriter* xw)
+void Weather(XMLWriter xw)
 {
-  xw->comment("The weather in Nebraska");
-  xw->tagOpen("Weather");
-  xw->writeNode("Date", "20131106");
-  xw->writeNode("Time", "11:42");
-  xw->writeNode("Temp", "23.4");
-  xw->writeNode("Humi", "50%");
-  xw->writeNode("Rain", "10mm");
-  xw->writeNode("Sun", "40");
-  xw->tagClose();
+  xw.comment("The weather in Nebraska");
+  xw.tagOpen("Weather");
+  xw.writeNode("Date", "20131106");
+  xw.writeNode("Time", "11:42");
+  xw.writeNode("Temp", "23.4");
+  xw.writeNode("Humi", "50%");
+  xw.writeNode("Rain", "10mm");
+  xw.writeNode("Sun", "40");
+  xw.tagClose();
 }
 
-// casting to keep some compilers happy
-void AnalogPorts(XMLWriter* xw, const char* name)
+
+void AnalogPorts(XMLWriter xw, const char* name)
 {
   char buffer[24];
 
-  xw->comment("The analog ports are multiplexed");
-  xw->tagOpen("Analog", name);
-  xw->writeNode("Analog0", itoa(analogRead(A0), buffer, 10));
-  xw->writeNode("Analog1", (uint16_t) analogRead(A1));
-  xw->writeNode("Analog2", (double) (5.0 * analogRead(A2)) / 1023); // default nr decimals = 2
-  xw->writeNode("Analog3", (double) (5.0 * analogRead(A2)) / 1023, (uint8_t)3);
-  xw->tagClose();
+  xw.comment("The analog ports are multiplexed");
+  xw.tagOpen("Analog", name);
+  xw.writeNode("Analog0", itoa(analogRead(A0), buffer, 10));
+  xw.writeNode("Analog1", analogRead(A1));
+  xw.writeNode("Analog2", (5.0 * analogRead(A2)) / 1023); // default nr decimals = 2
+  xw.writeNode("Analog3", (5.0 * analogRead(A2)) / 1023, 3);
+  xw.tagClose();
 }
 
-// casting to keep some compilers happy
-void DigitalPorts(XMLWriter* xw)
+void DigitalPorts(XMLWriter xw)
 {
   char buffer[24];
 
-  xw->comment("The digital ports are not multiplexed");
-  xw->tagOpen("Digital");
-  xw->writeNode("D1", itoa(digitalRead(1), buffer, 10));
-  xw->writeNode("D13", (uint8_t) digitalRead(13));
-  xw->tagClose();
+  xw.comment("The digital ports are not multiplexed");
+  xw.tagOpen("Digital");
+  xw.writeNode("D1", itoa(digitalRead(1), buffer, 10));
+  xw.writeNode("D13", digitalRead(13));
+  xw.tagClose();
 }
 
-void DataTypes(XMLWriter* xw)
+void DataTypes(XMLWriter xw)
 {
-  xw->comment("Testing dataTypes I");
-  xw->tagOpen("Datatypes");
-  xw->writeNode("BoolT", 1 == 1);
-  xw->writeNode("BoolF", 1 == 0);
-  xw->writeNode("BIN", 42, BIN);
-  xw->writeNode("DEC", 42, DEC);
-  xw->writeNode("HEX", 42, HEX);
-  xw->writeNode("OCT", 42, OCT);
-  xw->tagClose();
-
-  xw->comment("Testing dataTypes II");
+  xw.comment("Testing dataTypes I");
+  xw.tagOpen("Datatypes");
+  xw.writeNode("BoolT", 1 == 1);
+  xw.writeNode("BoolF", 1 == 0);
+  xw.writeNode("BIN", 42, BIN);
+  xw.writeNode("DEC", 42, DEC);
+  xw.writeNode("HEX", 42, HEX);
+  xw.writeNode("OCT", 42, OCT);
+  xw.tagClose();
+  
+  xw.comment("Testing dataTypes II");
   for (int i = 0; i < 3; i++)
   {
-    xw->tagStart("dataTypes");
-    xw->tagField("BoolT", 1 == 1);
-    xw->tagField("BoolF", 1 == 0);
+    xw.tagStart("dataTypes");
+    xw.tagField("BoolT", 1 == 1);
+    xw.tagField("BoolF", 1 == 0);
     int x = analogRead(A0);
-    xw->tagField("BIN", x, BIN);
-    xw->tagField("DEC", x, DEC);
-    xw->tagField("HEX", x, HEX);
-    xw->tagField("OCT", x, OCT);
-    xw->tagEnd();
+    xw.tagField("BIN", x, BIN);
+    xw.tagField("DEC", x, DEC);
+    xw.tagField("HEX", x, HEX);
+    xw.tagField("OCT", x, OCT);
+    xw.tagEnd();
   }
 }
 
